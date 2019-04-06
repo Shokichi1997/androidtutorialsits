3235ff7d90021fd11d0972d282c89262c3d7736e
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-04-06 22:35:48 +0700

    Update sign_in.php

--- api/sign_in.php
+++ api/sign_in.php
@@ -4,50 +4,50 @@ include "../lib/data.php";
 //Test
 $res = null;
 if (isset($_POST["email"]) && isset($_POST["password"])) {
-//     $password = $_POST["password"];
-//     $email = $_POST["email"];
+     $password = $_POST["password"];
+     $email = $_POST["email"];
     
-//     $sql = "SELECT * FROM public.user WHERE email ='$email'";
-//     // ket noi database
-//     include "../lib/db.php";
-//     $dbconnection = new postgresql("");
-//     if($dbconnection->$dbconnection->isValid())
-//     {
-//       $result = $dbconnection->select($sql);
-//       if ($result !== null) {
-//         if (pg_num_rows($result) > 0) {
-//             //user exist, check password
-//             $dbpassword = null;
-//             $user = null;
-//             while ($data = pg_fetch_object($result)) {
-//                 $dbpassword = $data->password;
-//                 $user = $data;
-//                 break;
-//             }
-//             if ($dbpassword !== null) {
-//                 if (strcasecmp($dbpassword, $password) == 0) {
-//                     $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
-//                     unset($user->password);
-//                     $res->data = $user;
-//                 } else {
-//                     $res = new Result(Constant::INVALID_PASSWORD, 'Password is not matching.');
-//                 }
-//             } else {
-//                 $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-//             }
-//         } else {
-//             $res = new Result(Constant::INVALID_USER, 'User is not exist');
-//         }
-//         $dbconnection->closeResult($result);
-//       } else {
-//           $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-//       }
-//       $dbconnection->close();
-//     }//$dbconnection->$dbconnection->isValid()
-//     else
-//     {
-//       $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
-//     }
+    $sql = "SELECT * FROM public.user WHERE email ='$email'";
+    // ket noi database
+    include "../lib/db.php";
+    $dbconnection = new postgresql("");
+    if($dbconnection->isValid())
+    {
+      $result = $dbconnection->select($sql);
+      if ($result !== null) {
+        if (pg_num_rows($result) > 0) {
+            //user exist, check password
+            $dbpassword = null;
+            $user = null;
+            while ($data = pg_fetch_object($result)) {
+                $dbpassword = $data->password;
+                $user = $data;
+                break;
+            }
+            if ($dbpassword !== null) {
+                if (strcasecmp($dbpassword, $password) == 0) {
+                    $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');
+                    unset($user->password);
+                    $res->data = $user;
+                } else {
+                    $res = new Result(Constant::INVALID_PASSWORD, 'Password is not matching.');
+                }
+            } else {
+                $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+            }
+        } else {
+            $res = new Result(Constant::INVALID_USER, 'User is not exist');
+        }
+        $dbconnection->closeResult($result);
+      } else {
+          $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+      }
+      $dbconnection->close();
+    }//$dbconnection->$dbconnection->isValid()
+    else
+    {
+      $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
+    }
 } else {
     $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
 }
