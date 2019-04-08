2f7fc953b5de73a3e9ef673974e57d30b00350bd
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-04-08 22:45:01 +0700

    Update getTest.php

--- api/getTest.php
+++ api/getTest.php
@@ -19,35 +19,35 @@ if(isset($_GET['lesson_id'])){
           array_push($arr_question,$question);
         }
         
-//         foreach ($arr_question as $qs) {
-//           $question_id = $qs->question_id;
-//           $sql_answer = "SELECT answer_id,answer_content,result FROM public.answer WHERE question_id = '$question_id' order by random()";
-//           $result_answer = $dbconnection->select($sql_answer);
-//           if($result_answer!==null){
-//             if(pg_num_rows($result_answer)>0){
-//               $arr_answers = array();
-//               $answer = null;
-//               while($data = pg_fetch_object($result_answer)){
-//                 $answer->answer_id = $data->answer_id;
-//                 $answer->answer_content = $data->answer_content;
-//                 $answer->result = $data->result;
-//                 array_push($arr_answers,$answer);
-//               }
-//               $qs->answers = $arr_answers;
-//             }
-//             //pg_num_rows($result_answer)>0
-//             else{
-//               $res = new Result(Constant::GENERAL_ERROR, 'Question do not have answers.');
-//             }
-//           }
-//           //$result_answer!==null
-//           else{
-//             $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
-//           }
-//         }
-//         $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');   
-//         $res->data = $arr_question;
-//      //pg_num_rows($result)>0 
+        foreach ($arr_question as $qs) {
+          $question_id = $qs->question_id;
+          $sql_answer = "SELECT answer_id,answer_content,result FROM public.answer WHERE question_id = '$question_id' order by random()";
+          $result_answer = $dbconnection->select($sql_answer);
+          if($result_answer!==null){
+            if(pg_num_rows($result_answer)>0){
+              $arr_answers = array();
+              $answer = null;
+              while($data = pg_fetch_object($result_answer)){
+                $answer->answer_id = $data->answer_id;
+                $answer->answer_content = $data->answer_content;
+                $answer->result = $data->result;
+                array_push($arr_answers,$answer);
+              }
+              $qs->answers = $arr_answers;
+            }
+            //pg_num_rows($result_answer)>0
+            else{
+              $res = new Result(Constant::GENERAL_ERROR, 'Question do not have answers.');
+            }
+          }
+          //$result_answer!==null
+          else{
+            $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+          }
+        }
+        $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');   
+        $res->data = $arr_question;
+     //pg_num_rows($result)>0 
       }
       else{
         $res = new Result(Constant::GENERAL_ERROR, 'This lesson do not have questions.');
