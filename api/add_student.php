b78f16243ea66fed6f674aa099c7e82b3a8a9abf
buingocdanh97 <42201318+buingocdanh97@users.noreply.github.com>
2019-04-14 13:04:51 +0700

    update api add student

--- api/add_student.php
+++ api/add_student.php
@@ -1,25 +1,46 @@
-<?php 
-header('Content-Type: application/json');
-//error_reporting(E_ALL);
-include "../lib/data.php";
-$res = null;
 
-if(isset($_GET['user_id'])){
-  $user_id = $_GET['user_id'];
-  include "../lib/db.php";
+<?php
+header('Content-Type: application/json');
+include "../lib/data.php";
+$res    = null;
+$result =null;
+if(isset($_GET['student_code'])){
+  $student_code = $_GET['student_code'];
+  $password     = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+  $role         =  3; //default role 3(role is student)
+  $full_name    = "anymouse";    
+  //connect database
+  include ('../lib/db.php');
+  //check acount is exsit
+  $sql_find_user = "SELECT * FROM public.user WHERE studet_code = '$student_code'";
   $dbconnection = new postgresql("");
-  if($dbconnection->isValid()){
-    //Delete student form table user
-         $sql="DELETE FROM public.User WHERE user_id='$user_id' ";
-         $result = $dbconnection->execute($sql);
-         $res=new Result(Constant::SUCCESS,'Excute successfully');
-  }//$dbconnection->isValid()
-  else{
-    $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
+  if($dbconnection->isValid())
+  {
+     $result = $dbconnection->select($sql_find_user);
+      if($result!==null){
+        if(pg_num_rows($result)==0){  
+            $sql_dk = "INSERT INTO public.user(full_name,password,role,student_code,date_create)
+            values ('$full_name','$password','$role','$studet_code',CURRENT_DATE)";
+            $dbconnection->execute($sql_dk);
+            //Registered successfully => return user infor 
+            $res = new Result(Constant::SUCCESS,'Registered successfully');
+        }// Excute sql add student
+        else{
+           $res = new Result(Constant::USER_EXIST , 'User is exist');
+        }// User is exist
+        $dbconnection->closeResult($result);
+      }
+      else{
+        $res = new Result(Constant::GENERAL_ERROR, 'There was an error while processing request. Please try again later.');
+      }
+      $dbconnection->close();
   }
-}//isset($_POST['user_id'])))
+  else{ 
+    $res = new Result(Constant::INVALID_DATABASE , 'Database is invalid.');  
+   }
+ }
 else{
-  $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
+    $res = new Result(Constant::INVALID_PARAMETERS, 'Invalid parameters.');
 }
 echo (json_encode($res));
 ?>
