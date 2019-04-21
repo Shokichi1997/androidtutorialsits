c2d5250314fbeeeccd4ce8a71cd4e9a14ca6a186
Hoàng Văn Lâm <hoangvanlamcntt@gmail.com>
2019-04-21 17:16:03 +0700

    Update getExamples.php

--- api/getExamples.php
+++ api/getExamples.php
@@ -7,17 +7,13 @@ $res = null;
 include "../lib/db.php";
 $dbconnection = new postgresql("");
 if($dbconnection->isValid()){
-	$url = "https://itstutorials.000webhostapp.com/wp-content/uploads/";
     $sql = "SELECT name,java_code,xml_code,icon FROM PUBLIC.examples";
     $result = $dbconnection->select($sql);
     $res_exam = array();
     if($result!==null){
       	if(pg_num_rows($result)>0){
 		while($data = pg_fetch_object($result)){
-			$img = $data->icon;
-			$img2 = file_get_contents($url.$img);
-			$icon = base64_encode($img2);
-	        	array_push($res_exam, new Example($data->name,$data->java_code,$data->xml_code,$img2));
+	        	array_push($res_exam, new Example($data->name,$data->java_code,$data->xml_code,$data->icon));
 	        }	          
 	        $res = new Result(Constant::SUCCESS, 'Operation complete successfully.');   
 	        $res->data = $res_exam;
