4bd471c3a066af7db09a038da2918fc929d18ac8
Danh <buingocdanh97@gmail.com>
2019-04-29 10:52:39 +0700

    ud

--- api/update_student_info.php
+++ api/update_student_info.php
@@ -7,12 +7,12 @@ $result =null;
 
 if(isset($_POST['user_id']))
 {
-  $user_id=$_POST['user_id'];
+  $user_id=$_POST['user_id']
   
   //connect database
   include ('../lib/db.php');
   //check acount is exsit
- // $sql_find_user = "SELECT * FROM public.user WHERE user_id = '$user_id'";
+  $sql_find_user = "SELECT * FROM public.user WHERE user_id = '$user_id'";
   $dbconnection = new postgresql("");
   if($dbconnection->isValid())
   {
